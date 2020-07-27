ac3933b988ed041c67f5b0792cf1b5bc082aa2a2
martinkalanda <65572191+martinkalanda@users.noreply.github.com>
2020-07-28 10:32:37 +1200

    Add files via upload

---
+++ wage calculaor.py
@@ -0,0 +1,10 @@
+#Create variables
+hourly_rate = int(input("what is your hourly rate"))
+hours_worked = int(input("how many hours have you worked per week"))
+
+#Calculate wages based on hourly_rate and hours_worked
+wages = hourly_rate * hours_worked 
+
+print("your wages are ${} per week" .format(wages))
+
+
